73f8183fc547c29f29dc79e21a0a35739e5e2a6b
xianfeng92 <oppoa1001@hotmail.com>
2021-11-07 16:25:30 +0800

    CodingDeping2

---
+++ CodingDeping/GNU Binutils/GNU Binutils.cpp
@@ -0,0 +1,32 @@
+
+
+// !! GNU Binutils
+
+GNU Binary Utilities 或 binutils '是一整套的编程语言工具程序, 用来处理许多格式的目标文件'。
+
+一般来说，这些程序都很简单, 大部分的复杂性都存在于 Binary File Descriptor library 和 libopcodes 这些库里头。
+
+// !! 指令
+
+binutils 包括以下这些指令:
+
+1. as   汇编器
+
+2. ld   链接器
+
+3. gprof  	性能分析工具程序
+
+4. addr2line 从目标文件的虚拟地址获取文件的行号或符号
+
+5. ar   可以对静态库做创建、修改和取出的操作
+
+6. nm   显示目标文件内的符号
+
+7. objdump 显示目标文件的相关信息，亦可反汇编
+
+8. readelf  显示ELF文件的内容
+
+9. size 列出总体和section的大小
+
+10. strip 从目标文件中移除符号
+
