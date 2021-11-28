49a988be51da241d58edbde4f1840d24efb92cc5
xianfeng92 <oppoa1001@hotmail.com>
2021-11-28 15:06:49 +0800

    Cplusplus2

---
+++ C++PrimerV6/开始学习C++/myfirst.cpp
@@ -0,0 +1,11 @@
+// myfirst.cpp  -- display a message
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time" << endl;
+    cout << "you won't forget it" << endl;
+    return 0;
+}
